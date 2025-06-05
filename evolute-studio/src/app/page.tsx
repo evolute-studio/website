f650b146542e1558dddeef517be41562887e5f8e
sventime <semirazov94@gmail.com>
2025-06-05 19:11:58 +0100

    fixes

--- evolute-studio/src/app/page.tsx
+++ evolute-studio/src/app/page.tsx
@@ -56,27 +56,38 @@ export default function Home() {
       </div>
 
 
-      {/* Logo Dialog - Top Center */}
+      {/* Logo - Floating game banner style */}
       {showLogoDialog && (
-        <div className="fixed top-4 left-1/2 transform -translate-x-1/2 z-10">
-          <div className="border-4 rounded-lg pixel-dialog-shadow animate-slideIn" style={{ backgroundColor: '#2B2C45', borderColor: '#A47552' }}>
-            <div className="flex justify-center">
-              <Image
-                src="/wide.svg"
-                alt="Evolute Studio Logo"
-                width={400}
-                height={200}
-                className="pixel-art"
-              />
+        <div className="fixed top-6 left-1/2 transform -translate-x-1/2 z-10 animate-slideIn">
+          <div className="relative">
+            {/* Banner background with dialog color scheme */}
+            <div
+              className="transform -skew-x-12 border-4"
+              style={{
+                backgroundColor: '#2B2C45',
+                borderColor: '#A47552',
+                boxShadow: '6px 6px 0px #7A5939, 8px 8px 0px #5A4129, inset 0 2px 0 rgba(255,255,255,0.1), inset 0 -2px 0 rgba(0,0,0,0.3)'
+              }}
+            >
+              <div className="transform skew-x-12">
+                <Image
+                  src="/wide.svg"
+                  alt="Evolute Studio Logo"
+                  width={420}
+                  height={100}
+                  className="pixel-art filter brightness-110"
+                />
+              </div>
             </div>
+
           </div>
         </div>
       )}
 
       {/* Main Dialog - Bottom Center */}
       {showMainDialog && (
-        <div className="fixed top-2/3 left-1/2 transform -translate-x-1/2 z-10">
-          <div className="border-4 rounded-lg p-8 max-w-md pixel-dialog-shadow animate-slideIn" style={{ backgroundColor: '#2B2C45', borderColor: '#A47552' }}>
+        <div className="fixed top-3/4 left-1/2 transform -translate-x-1/2 z-10">
+          <div className="border-4 rounded-lg p-8 w-[500px] pixel-dialog-shadow animate-slideIn" style={{ backgroundColor: '#2B2C45', borderColor: '#A47552' }}>
             {/* Typewriter text */}
             <div className="text-white text-xl leading-relaxed mb-6 text-center text-outlined font-bold">
               <TypewriterText
